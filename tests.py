ae280c14d652a891185fe246dc2eb69faf81a262
pickDefault <orian755@gmail.com>
2024-02-10 11:03:54 +0200

    Initial commit

---
+++ tests.py
@@ -0,0 +1,106 @@
+from avl_template import AVLTree
+
+class tests:
+    
+    @staticmethod
+    def test1():
+        tree=AVLTree()
+
+
+        tree.insert(6,",")
+        tree.insert(7,",")
+
+        #
+        # print(tree.get_root().get_key())
+        tree.delete(tree.get_root())
+        # print(tree.get_root().get_key())
+        tree.delete(tree.get_root())
+        # print(tree.get_root())
+        tree.insert(6,",")
+        # print(tree.get_root().get_key())
+        # print(tree.get_root().get_left().get_key())
+        # print(tree.get_root().get_right().get_key())
+    
+    @staticmethod
+    def testInsertDelete():
+        tree = AVLTree()
+
+        keys = [23, 4, 30, 11, 7, 15, 40, 43, 2, 1]
+        
+        # test RL rotation
+        tests.insert_array(tree, keys[0:5])
+        tests.assert_neighbors(tree, 7, 4, 11) 
+
+        # test LR rotation
+        tests.insert_array(keys[5:6], keys[5:6]) # insert 15
+        tests.assert_neighbors(tree, 15, None, None)
+        tests.assert_neighbors(tree, 23, 15, 30)
+        tests.assert_neighbors(tree, 11, 7, 23)
+        tests.assert_neighbors(tree, 7, 4, None)
+        tests.test_root(tree, 11)
+        
+        # test L rotation
+        print(f"CHECK KEYS {keys[6:8]}")
+        tests.insert_array(tree, keys[6:8]) # insert 40, 43
+        tests.assert_neighbors(tree, 40, 30, 43)
+        tests.assert_neighbors(tree, 23, 15, 40)
+        
+        # tests R rotation
+        print(f"CHECK KEYS {keys[8:10]}")
+        tests.insert_array(tree, keys[8:10]) # insert 2, 1
+        tests.assert_neighbors(tree, 4, 2, 7)
+        tests.assert_neighbors(tree, 2, 1, None)
+        tests.assert_neighbors(tree, 7, None, None)
+        tests.assert_neighbors(tree, 11, 4, 23)
+        tests.test_root(tree, 11)
+        
+        # test deletions
+        tests.test_deletion(tree, 1, 0)
+        tests.assert_neighbors(tree, 4, 2, 7)
+        tests.assert_neighbors(tree, 2, None, None)
+        tests.assert_neighbors(tree, 7, None, None)
+        tests.assert_neighbors(tree, 11, 4, 23)
+        tests.test_root(tree, 11)
+        
+        tests.test_deletion(tree, 2, 0)
+        tests.assert_neighbors(tree, 4, None, 7)
+        tests.assert_neighbors(tree, 7, None, None)
+        tests.assert_neighbors(tree, 11, 4, 23)
+        tests.test_root(tree, 11)
+        
+        # test L rotation
+        tests.test_deletion(tree, 7, 1)
+        tests.assert_neighbors(tree, 11, 4, 15)
+        tests.assert_neighbors(tree, 23, 11, 40)
+        tests.test_root(tree, 23)
+        
+        # TODO: empty the tree and test to see everything is correct
+        
+    @staticmethod
+    def assert_neighbors(tree, node_key, left_key, right_key):
+        node = tree.search(node_key)
+        
+        assert node.get_right() is tree.search(right_key), \
+            f"Checking neighbors for {node_key}, right neighbor is {node.get_right()} but search returned something else when searching for key {right_key}"
+        assert node.get_left() is tree.search(left_key), \
+            f"Checking neighbors for {node_key}, left neighbor is {node.get_left()} but search returned something else when searching for key {left_key}"
+        
+    @staticmethod
+    def insert_array(tree, key_array):
+        for key in key_array:
+            tree.insert(key, key)
+            
+    @staticmethod
+    def test_deletion(tree, deletion_key, num_balancing_actions):
+        deletion_node = tree.search(deletion_key)
+        check_balacing_actions = tree.delete(deletion_node)
+
+        assert tree.search(deletion_key) is None, \
+            f"Deleting {deletion_key}, searching for deleted key did not return None"
+        assert check_balacing_actions == num_balancing_actions, \
+            f"Deleting {deletion_key}, deletion returned {check_balacing_actions} balancing actions instead of {num_balancing_actions}"
+    
+    @staticmethod
+    def test_root(tree, root_key):
+        assert tree.get_root() is tree.search(root_key), \
+            f"Root is {tree.get_root()}, but search returned something else when searching for {root_key}"    
