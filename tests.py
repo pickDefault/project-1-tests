aa2023e2e9b1fa7c5e73a09c971b434d031ed4ed
pickDefault <orian755@gmail.com>
2024-02-12 08:54:38 +0200

    Fixed join tests, added deletion test, added split tests

--- tests.py
+++ tests.py
@@ -55,6 +55,11 @@ class tests:
         testHelper.assert_neighbors(tree, 23, 11, 40,7)
         testHelper.test_root(tree, 23)
         
+        testHelper.test_deletion(tree, 23, 1)
+        testHelper.assert_neighbors(tree, 30, 11, 40)
+        testHelper.assert_neighbors(tree, 11, 4, 15)
+        testHelper.assert_neighbors(tree, 40, None, 43)
+        
         # TODO: empty the tree and test to see everything is correct
         
     @staticmethod
@@ -71,7 +76,8 @@ class tests:
         tree_tuple = testHelper.test_join_helper([4, 2, 5], 
                                                  [20, 22, 16, 18, 14], 
                                                  10, 
-                                                 10)
+                                                 10,
+                                                 2)
         for tree in tree_tuple:
             #  tests for trees [4, 2, 5] and [20, 22, 16, 18, 14]
             #  balancing should be called on node c and node x
@@ -84,12 +90,53 @@ class tests:
         tree_tuple = testHelper.test_join_helper([4, 2, 5], 
                                                  [20, 22, 16],
                                                  10, 
-                                                 10)
+                                                 10,
+                                                 1)
         for tree in tree_tuple:
             testHelper.test_root(tree, 10)
             testHelper.assert_neighbors(tree, 10, 4, 20, 7)
             testHelper.assert_neighbors(tree, 20, 16, 22, 7)
             testHelper.assert_neighbors(tree, 4, 2, 5, 7)
+        
+        tree_tuple = testHelper.test_join_helper([],
+                                                 [],
+                                                 10,
+                                                 10,
+                                                 1)
+        
+        for tree in tree_tuple:
+            testHelper.test_root(tree, 10)
+            testHelper.assert_neighbors(tree, 10, None, None, 1)
+            
+        tree_tuple = testHelper.test_join_helper([12],
+                                                 [],
+                                                 10,
+                                                 10,
+                                                 2)
+        
+        for tree in tree_tuple:
+            testHelper.test_root(tree, 12)
+            testHelper.assert_neighbors(tree, 12, 10, None, 2)
+            
+    @staticmethod
+    def test_split():
+        
+        t1, t2 = testHelper.test_split_helper([20], 20)
+        
+        assert t1.get_root() is None and t2.get_root() is None, \
+            f"Splitting a tree with only one node did not return two empty trees"
+
+        t1, t2 = testHelper.test_split_helper([20, 10, 12, 8, 9, 25, 23, 27],
+                                              12)
+        
+        testHelper.assert_neighbors(t1, 9, 8, 10, 3)
+        testHelper.assert_neighbors(t2, 23, 20, 25, 4)
+        testHelper.assert_neighbors(t2, 25, None, 27, 4)
+        
+        t1, t2 = testHelper.test_split_helper([20, 10, 12, 8, 9, 25, 23, 27],
+                                25)
+        # add tests
+        
     
 class testHelper:
     # Helper functions for tests class. No need to use any of them.
@@ -108,7 +155,7 @@ class testHelper:
         assert node_left is left_result, \
             f"Checking neighbors for {node_key}, left neighbor is {node_left} but search returned something else when searching for key {left_key}"
         assert tree.size()==size, \
-            f"size error"
+            f"Expected tree size to be {size} but got {tree.size()} instead"
         
     @staticmethod
     def insert_array(tree, key_array):
@@ -118,17 +165,17 @@ class testHelper:
     @staticmethod
     def test_deletion(tree, deletion_key, num_balancing_actions):
         deletion_node = tree.search(deletion_key)
-        check_balacing_actions = tree.delete(deletion_node)
+        check_balancing_actions = tree.delete(deletion_node)
 
         assert tree.search(deletion_key) is None, \
             f"Deleting {deletion_key}, searching for deleted key did not return None"
-        assert check_balacing_actions == num_balancing_actions, \
-            f"Deleting {deletion_key}, deletion returned {check_balacing_actions} balancing actions instead of {num_balancing_actions}"
+        assert check_balancing_actions == num_balancing_actions, \
+            f"Deleting {deletion_key}, deletion returned {check_balancing_actions} balancing actions instead of {num_balancing_actions}"
     
     @staticmethod
     def test_root(tree, root_key):
         assert tree.get_root() is tree.search(root_key), \
-            f"Root is {tree.get_root()}, but search returned something else when searching for {root_key}"    
+            f"Root is {tree.get_root()}, but search returned something else when searching for expected root key {root_key}"    
     
     """
     @param tree: tree to test
@@ -143,15 +190,23 @@ class testHelper:
         assert expectedArray == avl_to_array_result, \
             f"Expected avl_to_array() to return \n{expectedArray}\nbut got\n{avl_to_array_result}"
 
+    """
+    tests joining two trees in either order (tree1 to tree2 or tree2 to tree1)
+    @param key_array1: keys in one of the trees
+    @param key_array2: keys in one of the trees
+    @returns: a tuple of trees where the first tree was joined in one order and the second was joined in another
+    """
     @staticmethod
-    def test_join_helper(key_array1, key_array2, joiningNodeKey, joiningNodeValue):
+    def test_join_helper(key_array1, key_array2, joiningNodeKey, joiningNodeValue, expectedDiff):
         tree1 = AVLTree()
         tree2 = AVLTree()
 
         testHelper.insert_array(tree1, key_array1)
         testHelper.insert_array(tree2, key_array2)
         
-        tree1.join(tree2, joiningNodeKey, joiningNodeValue)
+        actualDiff = tree1.join(tree2, joiningNodeKey, joiningNodeValue)
+        assert actualDiff == expectedDiff, \
+            f"Expected join to return {expectedDiff} but got {actualDiff} instead"
 
         tree1_flip_order = AVLTree()
         tree2_flip_order = AVLTree()
@@ -159,6 +214,22 @@ class testHelper:
         testHelper.insert_array(tree1_flip_order, key_array2)
         testHelper.insert_array(tree2_flip_order, key_array1)
         
-        tree1_flip_order.join(tree2_flip_order, joiningNodeKey, joiningNodeValue)
+        actualDiff = tree1_flip_order.join(tree2_flip_order, joiningNodeKey, joiningNodeValue)
+        assert actualDiff == expectedDiff, \
+            f"With flipped order of trees in join, expected join to return {expectedDiff} but got {actualDiff} instead"
 
         return [tree1, tree1_flip_order]
+
+    """splits a tree with keys key_array by split_node_key, throws error
+    if split_node_key is not in key_array and wasn't found in the tree
+    """
+    @staticmethod
+    def test_split_helper(key_array, split_node_key):
+        tree = AVLTree()
+        testHelper.insert_array(tree, key_array)
+        split_node_key = tree.search(split_node_key)
+        
+        assert split_node_key is not None, \
+            f"Key {split_node_key} was not found by search"
+        
+        return tree.split(split_node_key)
